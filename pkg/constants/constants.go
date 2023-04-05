fe97b0a233cbdd088c181a41af4ff477bf0b0a5a
prathusingh <prathusingh21@gmail.com>
2023-04-05 18:51:16 -0400

    init config constants

---
+++ pkg/constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	GrpcPort = "GRPC_PORT"
+	GraphQlPort = "GRAPHQL_PORT"
+	KafkaBrokers = "KAFKA_BROKERS"
+	RedisAddr = "REDDIS_ADDR"
+	MongoDbURI = "MONGO_URI"
+	PostgresqlHost = "POSTGRES_HOST"
+	PostgresqlPort = "POSTGRES_PORT"
+)
